9cfb143b20ce5393d8f033ba5d1eacc0caabb618
federico arias <fede.ariass33@gmail.com>
2022-09-22 20:09:58 -0300

    adding arrow function

--- archivo-prueba.js
+++ archivo-prueba.js
@@ -1,6 +1,6 @@
-const nombres = ['federico', 'arias', 'nahuel'];
-
-
-for(let i= 0; i < 10; i++){
-    console.log('hola mi nombre es', nombres[0], nombres[2], nombres[1]);
+const nombre = (a, b)=>{
+    console.log(a + b);
 }
+
+
+nombre('federico', ' arias');
